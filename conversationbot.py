4df789f1c68a4a7aa152d644f7fa84bbe9824b7f
ivanrputra <ivanrputra@gmail.com>
2019-07-29 17:12:19 +0700

    added splicing text, minus insert to db

--- conversationbot.py
+++ conversationbot.py
@@ -19,7 +19,7 @@ bot.
 """
 
 
-
+import pymysql.cursors
 import logging
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
@@ -32,60 +32,179 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-BIO = range(4)
+BIO,VALDAT_MAINCORE,ODP_LOCATION,ODC_LOCATION = range(4)
 
+def connection():
+    conn = pymysql.connect('10.112.82.94','ikrom','akuadmindb','valdat_test')
+    cursor = conn.cursor()
+    return cursor
 
 def start(update, context):
     user = update.message.from_user
     
     update.message.reply_text('''
-        ODP-BLB-FBM/12 
-        DS 3 KAP 12 CORE 6&7
-        (odc/distribusi berapa/nomer odp.01) 
-        FBM/D03/13.01
-        5 SPL-C KAP 8
-        QRCODE ODP
-        QRCODE PORT
-        ALAMAT
-        KELURAHAN
-        KECAMATAN
-        KOORDINAT (lat,long)
-        TO
-        ODC-BLB-FBM KAP 144
-        KOORDINAT (lat,long)
-        IN
-        OTB 1 PORT 5 CORE 5
-        TO
-        SPL-B 5 PORT 1&2
-        TO
-        OTB 9 PORT 6&7 CORE 6&7
-        KET : FEEDER LOSS
-
-        ODC KAP 144=12 PORT PANEL IN & OUT
-        ODC KAP 48=24 PANEL IN & OUT
-
-        baris 0 : nama odp
-        baris 2 : nama odc
-        baris 4 : nomer otb, port, core
-        baris 6 : nomer spl, port (bisa 2 core, dipisah pake &)
-        baris 8 : nomer otb, port, core (port dan core bisa 2 core, dipisah pake &)
-        baris 9 : keterangan
-        ''')
-    return BIO
+ODP-BLB-FBM/12
+DS 3 KAP 12 CORE 6&7
+FBM/D03/13.01
+5 SPL-C KAP 8
+QRCODE ODP
+QRCODE PORT
+ALAMAT
+KELURAHAN
+KECAMATAN
+TO
+ODC-BLB-FBM KAP 144
+IN
+OTB 1 PORT 5 CORE 5
+TO
+SPL-B 5 PORT 1&2
+TO
+OTB 9 PORT 6&7 CORE 6&7
+KET : FEEDER LOSS
+''')
+    return VALDAT_MAINCORE
 
 
-def bio(update, context):
+def valdat_maincore(update, context):
+    # data = {}
+    # context.user_data = {}
+    context.user_data.clear()
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
+    split_message = update.message.text.splitlines()
 
+    if len(split_message) != 18:
+        update.message.reply_text('Input anda kurang atau berlebih silahkan ulangi lagi /start')
+        return ConversationHandler.END        
+
+    #
+    distribusi                  = split_message[1].split()
+    splitter                    = split_message[3].split()
+    odc                         = split_message[10].split()
+    odc_in                      = split_message[12].split()
+
+    #
+    odc_split                   = split_message[14].split()
+    #
+    odc_out                     = split_message[16].split()
+
+    d_core                      = {}
+    odc_out_port                = {}
+    odc_out_core                = {}
+    odc_splt_out                = {}
+
+    if len(distribusi[5]) == 1:
+        d_core                  = distribusi[5]
+        odc_out_port            = odc_out[3]
+        odc_out_core            = odc_out[5]
+        odc_splt_out            = odc_split[3]
+    elif len(distribusi[5]) >= 1:
+        if len(distribusi[5].split('&')) != len(odc_out[3].split('&')) or len(distribusi[5].split('&')) != len(odc_out[5].split('&')) or len(distribusi[5].split('&')) != len(odc_split[3].split('&')):
+            update.message.reply_text('Jumlah core pada odp distribusi dan odc output tidak sama, silahkan ulang lagi /start')
+            return ConversationHandler.END
+        d_core                  = distribusi[5].split('&')
+        odc_out_port            = odc_out[3].split('&')
+        odc_out_core            = odc_out[5].split('&')
+        odc_splt_out            = odc_split[3].split('&')
+    
+    for x in range(len(d_core)):
+        detail = {}
+
+        detail['odp_name']            = split_message[0]
+        detail['distribusi_ke']       = distribusi[1]
+        detail['distribusi_kap']      = distribusi[3]
+        detail['distribusi_core']     = d_core[x]
+        detail['odp_index']           = split_message[2] 
+        detail['splitter_no']         = splitter[0]
+        detail['splitter_name']       = splitter[1]
+        detail['splitter_kap']        = splitter[3]
+        detail['odp_qrcode']          = split_message[4]
+        detail['odp_port']            = split_message[5]
+        detail['odp_address']         = split_message[6]
+        detail['odp_kelurahan']       = split_message[7]
+        detail['odp_kecamatan']       = split_message[8]
+        detail['odc_name']            = odc[0]
+        detail['odc_kap']             = odc[2]
+        detail['in_tray']             = odc_in[1]
+        detail['in_port']             = odc_in[3]
+        detail['in_core']             = odc_in[5]
+        detail['splt_name']           = odc_split[0]+'.1-'+odc_split[1]
+        detail['splt_out']            = odc_splt_out[x]
+        detail['out_tray']            = odc_out[1]
+        detail['out_port']            = odc_out_port[x]
+        detail['out_core']            = odc_out_core[x]
+        detail['description']         = split_message[17]
+
+        # data[x] = detail
+        context.user_data[x] = detail
+    logger.info(context.user_data)
+    update.message.reply_text('Masukkan koordinat ODC')
+
+    return ODC_LOCATION
+
+def odc_location(update, context):
+    user = update.message.from_user
+    user_location = update.message.location
+    location = {}
+    location['odc_lat']             = user_location.latitude
+    location['odc_long']            = user_location.longitude
+    for x in range(len(context.user_data)):
+        context.user_data[x].update(location)
+    
+    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                user_location.longitude)
+
+    update.message.reply_text('Masukkan koordinat ODP')
+
+    return ODP_LOCATION
+
+def odp_location(update, context):
+    cursor = connection()
+    # cursor.execute("insert into `odc` ")
+    # res = cursor.fetchone()
+
+    
+    user = update.message.from_user
+    user_location = update.message.location
+
+    location = {}
+    location['odp_lat']             = user_location.latitude
+    location['odp_long']            = user_location.longitude
+    for x in range(len(context.user_data)):
+        context.user_data[x].update(location)
+
+    data = context.user_data
+    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                user_location.longitude)
+    data = context.user_data 
+    for x in range(len(data)):
+        update.message.reply_text(data[x])
+        # update.message.reply_text(data[x]['odp_name']+"\n"+
+        # "DS "+data[x]['distribusi_ke']+" KAP "+data[x]['distribusi_kap']+" CORE "+data[x]['distribusi_core']+"\n")
+    # print(update.data)        
+# 5 SPL-C KAP 8
+# QRCODE ODP
+# QRCODE PORT
+# ALAMAT
+# KELURAHAN
+# KECAMATAN
+# TO
+# ODC-BLB-FBM KAP 144
+# IN
+# OTB 1 PORT 5 CORE 5
+# TO
+# SPL-B 5 PORT 1&2
+# TO
+# OTB 9 PORT 6&7 CORE 6&7
+# KET : FEEDER LOSS
+# )
+    update.message.reply_text('Terima Kasih Anda telah berhasil input Validasi Maincore, klik /start untuk validasi lagi')
+    connection.close()
     return ConversationHandler.END
-
 
 def cancel(update, context):
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
+    logger.info("User %s canceled the validation.", user.first_name)
+    update.message.reply_text('Anda Telah Membatalkan Validasi, ulangi /start',
                               reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
@@ -95,6 +214,28 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def format(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('''
+ODP-BLB-FBM/12
+DS 3 KAP 12 CORE 6&7
+FBM/D03/13.01
+5 SPL-C KAP 8
+QRCODE ODP
+QRCODE PORT
+ALAMAT
+KELURAHAN
+KECAMATAN
+TO
+ODC-BLB-FBM KAP 144
+IN
+OTB 1 PORT 5 CORE 5
+TO
+SPL-B 5 PORT 1&2
+TO
+OTB 9 PORT 6&7 CORE 6&7
+KET : FEEDER LOSS
+''')
 
 def main():
     # Create the Updater and pass it your bot's token.
@@ -110,13 +251,17 @@ def main():
         entry_points=[CommandHandler('start', start)],
 
         states={
-            BIO: [MessageHandler(Filters.text, bio)]
+            VALDAT_MAINCORE: [MessageHandler(Filters.text, valdat_maincore)],
+
+            ODC_LOCATION: [MessageHandler(Filters.location, odc_location)],
+            ODP_LOCATION: [MessageHandler(Filters.location, odp_location)],
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
     dp.add_handler(conv_handler)
+    dp.add_handler(CommandHandler("format", format))
 
     # log all errors
     dp.add_error_handler(error)
@@ -132,3 +277,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
